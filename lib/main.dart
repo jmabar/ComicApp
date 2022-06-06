16b870f3a6e00a85a489b13505e444335d9a02b5
Juan Maria Barriola <barriolajuanm@gmail.com>
2022-06-06 16:28:12 -0300

    Main - No tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,33 @@
+import 'package:comic_app/business_logic/blocs/connectivity/connectivity_bloc.dart';
+import 'package:comic_app/data/repositories/comic_vine_repository.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'business_logic/blocs/typeview/typeview_bloc.dart';
+import 'presentation/pages/pages.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const ComicVineApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ComicVineApp extends StatelessWidget {
+  const ComicVineApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => MainTypeviewBloc(ComicVineRepository())),
+        BlocProvider(create: (_) => ConnectivityBloc()),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Comic Vine App',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MainPage()),
     );
   }
 }
