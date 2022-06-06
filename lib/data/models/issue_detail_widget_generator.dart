56e33a5a375ae595f254275e67379bfe9b8c1c30
Juan Maria Barriola <barriolajuanm@gmail.com>
2022-06-06 17:40:15 -0300

    Side by side issue data layout

--- lib/data/models/issue_detail_widget_generator.dart
+++ lib/data/models/issue_detail_widget_generator.dart
@@ -10,7 +10,19 @@ class IssueDetailWidgetGenerator {
     widgets.add(Text(title));
     widgets.add(const Divider(height: 2.0, thickness: 2.0));
     widgets.add(const SizedBox(height: 20.0));
-    widgets.addAll(generateRowWidgetsSideBySide(issueDataObjects));
+    widgets.add(Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [...generateRowWidgetsSideBySide(issueDataObjects, true)],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [...generateRowWidgetsSideBySide(issueDataObjects, false)],
+        )
+      ],
+    ));
 
     return widgets;
   }
@@ -27,22 +39,35 @@ class IssueDetailWidgetGenerator {
   }
 
   //Generates Row Widgets given a list of IssueData objects
-  static List<Widget> generateRowWidgetsSideBySide(List<dynamic> issueDataObjects) {
-    List<Widget> rowsWidget = [];
-    for (int i = 0; i < issueDataObjects.length; i += 2) {
-      final Widget rowWidget = Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          DataContainer(data: issueDataObjects[i].name!),
-          (i + 1) < issueDataObjects.length ? DataContainer(data: issueDataObjects[i + 1].name!) : Container(),
-        ],
-      );
-
-      rowsWidget.add(rowWidget);
-      rowsWidget.add(const SizedBox(height: 12.0));
+  static List<Widget> generateRowWidgetsSideBySide(List<dynamic> issueDataObjects, bool isFirstColumn) {
+    List<Widget> columnChildrens = [];
+    if (isFirstColumn) {
+      for (int i = 0; i < issueDataObjects.length; i += 2) {
+        columnChildrens.add(DataContainer(data: issueDataObjects[i].name!));
+        columnChildrens.add(const SizedBox(height: 8.0));
+      }
+      return columnChildrens;
+    } else {
+      for (int i = 1; i < issueDataObjects.length; i += 2) {
+        columnChildrens.add(DataContainer(data: issueDataObjects[i].name!));
+        columnChildrens.add(const SizedBox(height: 8.0));
+      }
+      return columnChildrens;
     }
+    // for (int i = 0; i < issueDataObjects.length; i += 2) {
+    //   final Widget rowWidget = Row(
+    //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //     children: [
+    //       DataContainer(data: issueDataObjects[i].name!),
+    //       (i + 1) < issueDataObjects.length ? DataContainer(data: issueDataObjects[i + 1].name!) : Container(),
+    //     ],
+    //   );
 
-    return rowsWidget;
+    //   rowsWidget.add(rowWidget);
+    //   rowsWidget.add(const SizedBox(height: 12.0));
+    // }
+
+    // return rowsWidget;
   }
 
   static List<Widget> generateRowWidgetsOnePerRow(List<dynamic> issueDataObjects) {
