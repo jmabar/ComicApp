4e8b312e94a8eb62a985468ea2005108fb20efb5
Juan Maria Barriola <barriolajuanm@gmail.com>
2022-06-06 16:27:45 -0300

    Utility: Constants

---
+++ lib/util/constants.dart
@@ -0,0 +1,21 @@
+class Constants {
+  //Four grid view
+  static const double fourGridImageHeight = 235.0;
+  static const double fourGridImageWidth = 178.0;
+  static const double fourGridMainAxisExtent = 300;
+
+  //Two grid view
+  static const double twoGridImageHeight = 454.0;
+  static const double twoGridImageWidth = 339.0;
+  static const double twoGridMainAxisExtent = 400;
+
+  //List view
+  static const double listImageHeight = 721.0;
+  static const double listImageWidth = 537.0;
+  static const double listMainAxisExtent = 950;
+
+  //Responsive width limits
+  static const double mobileMainPageMaxWidth = 600;
+  static const double tableMainPageMaxWidth = 700;
+  static const double mobileIssueDetailPageMaxWidth = 680;
+}
