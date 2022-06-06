c545d7129bdb16d5f0a0f1f28f3e69a0bc49f291
Juan Maria Barriola <barriolajuanm@gmail.com>
2022-06-06 16:28:02 -0300

    UI Layer: Widgets Barrel File

---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'icon_with_text.dart';
+export 'issues_list_view.dart';
+export 'issues_grid_view.dart';
+export 'issue_image.dart';
+export 'detail_data_container.dart';
+export 'detail_backbtn_widget.dart';
